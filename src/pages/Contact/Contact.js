3d51c6c15d53933af9c710ea07269f9e75f6c32f
summenurfatema <shadrinmoni15@gmail.com>
2023-02-09 11:32:29 +0600

    contact section 40% completed

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -41,9 +41,7 @@ const Contact = () => {
 </div>
 </div>
 {/* <div>
-  <p className="tet-4 items-center">
-om/3d/free/thumb/github-5622954-468"h-36 w-36" alt='' />
-  <img src='https://cdn3d.iconscoree/thumb/linkedin-logo-5476201-4602452.png' className="h-36 w-36 animate-waving-hand" alt='' />
+  <p className="tet-4 items-cent-5476201-4602452.png' className="h-36 w-36 animate-waving-hand" alt='' />
 
   </div> */}
 </div>
