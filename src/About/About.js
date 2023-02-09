61a462fed42986db0af863e16c86524f8b5c20b6
summenurfatema <shadrinmoni15@gmail.com>
2023-02-09 18:27:41 +0600

    about section 50% completed

--- src/About/About.js
+++ src/About/About.js
@@ -23,6 +23,8 @@ const About = () => {
     coffee cups, and many sustainable products are items that are not unsafe to the climate.
       the market. The other alternatives are expensive and lack quality.</p>
 </div>
+
+
 {/* right side */}
 <div  className='w-full md:w-2/3 relative'>
     <div className='bg-green-300 h-80 w-72 absolute md:-top-24 md:left-2'></div>
@@ -32,9 +34,6 @@ const About = () => {
 
 
 {/* bottom */}
-
-
-
 
 
         </div>
